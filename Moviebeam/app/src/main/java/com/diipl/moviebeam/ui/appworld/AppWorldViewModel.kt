ceac492c28b478a3c2a40160bc1377bbe6865e92
siddheshh <siddheshh@diipl.com>
2024-01-08 19:09:21 +0530

    Datasource Integrated...

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/appworld/AppWorldViewModel.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/appworld/AppWorldViewModel.kt
@@ -1,16 +1,21 @@
 package com.diipl.moviebeam.ui.appworld
 
+import androidx.datastore.core.DataStore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.diipl.moviebeam.Constants
 import com.diipl.moviebeam.R
 import com.diipl.moviebeam.data.Resource
+import com.diipl.moviebeam.data.dto.datetime.DateTimeResponse
 import com.diipl.moviebeam.data.dto.theme.ThemeResponse
+import com.diipl.moviebeam.data.dto.weather.WeatherResponse
 import com.diipl.moviebeam.data.repositories.MovieBeamRepository
 import com.diipl.moviebeam.utils.SingleEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,11 +24,17 @@ class AppWorldViewModel @Inject constructor(
     private val movieBeamRepository: MovieBeamRepository
 ) : ViewModel() {
 
+    private val _dateTimeLiveData = MutableLiveData<Resource<DateTimeResponse>>()
+    val dateTimeLiveData: LiveData<Resource<DateTimeResponse>> get() = _dateTimeLiveData
+
     private val _themeLiveData = MutableLiveData<Resource<ThemeResponse>>()
     val themeLiveData: LiveData<Resource<ThemeResponse>> get() = _themeLiveData
 
+    private val _weatherLiveData = MutableLiveData<Resource<WeatherResponse>>()
+    val weatherLiveData: LiveData<Resource<WeatherResponse>> get() = _weatherLiveData
+
     init {
-        fetchThemeDetails("17205KKXLKF626")
+//        fetchThemeDetails("17205KKXLKF626")
     }
 
     private fun fetchThemeDetails(ua: String) {
@@ -34,6 +45,29 @@ class AppWorldViewModel @Inject constructor(
                 _themeLiveData.postValue(Resource.DataError(code = R.string.server_error))
             } else {
                 _themeLiveData.postValue(Resource.Success(response))
+            }
+        }
+    }
+
+    //------------------------fetching data from datasource--------------------
+    fun getThemeResponseData(dataStore: DataStore<ThemeResponse>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _themeLiveData.postValue(Resource.Loading())
+            dataStore.data.catch {
+                _themeLiveData.postValue(Resource.DataError(msg = Constants.SERVER_ERROR))
+            }.collect {
+                _themeLiveData.postValue(Resource.Success(it))
+            }
+        }
+    }
+
+    fun getWeatherResponseData(dataStore: DataStore<WeatherResponse>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _weatherLiveData.postValue(Resource.Loading())
+            dataStore.data.catch {
+                _weatherLiveData.postValue(Resource.DataError(msg = Constants.SERVER_ERROR))
+            }.collect {
+                _weatherLiveData.postValue(Resource.Success(it))
             }
         }
     }
