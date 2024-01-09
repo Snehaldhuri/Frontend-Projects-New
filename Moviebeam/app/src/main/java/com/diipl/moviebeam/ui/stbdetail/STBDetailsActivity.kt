af3f7251faa7ec8e35762fa0ef0f208213c06439
vaishnavi shimpi <wish75584@gmail.com>
2024-01-09 17:16:43 +0530

    implemented getstbMaster api

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/stbdetail/STBDetailsActivity.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/stbdetail/STBDetailsActivity.kt
@@ -1,12 +1,9 @@
 package com.diipl.moviebeam.ui.stbdetail
 
-import android.os.Build.VERSION_CODES.S
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.LiveData
-import com.diipl.moviebeam.R
 import com.diipl.moviebeam.data.Resource
 import com.diipl.moviebeam.data.dto.stbdetail.StbMasterResponse
 import com.diipl.moviebeam.databinding.ActivityStbdetailsBinding
@@ -15,8 +12,6 @@ import com.diipl.moviebeam.utils.SingleEvent
 import com.diipl.moviebeam.utils.observe
 import com.diipl.moviebeam.utils.setupSnackbar
 import com.diipl.moviebeam.utils.showToast
-import com.diipl.moviebeam.utils.toInvisible
-import com.diipl.moviebeam.utils.toVisible
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -62,6 +57,7 @@ class STBDetailsActivity : BaseActivity() {
                 }
                 // binding.pbLoader.toInvisible()
             }
+
             else -> {
                 status.errorCode?.let { stbDetailViewModel.showToastMessage(getString(it)) }
                 status.errorMsg?.let { stbDetailViewModel.showToastMessage(it) }
