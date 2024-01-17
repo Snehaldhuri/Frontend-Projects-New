47f696a96a208882bb370432a07327fc85623098
Siddhesh Hatiskar <siddheshh@diipl.com>
2024-01-17 16:24:44 +0530

    Fixes for Qr Code and cache Management in Casting.

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/casting/CastingActivity.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/casting/CastingActivity.kt
@@ -1,5 +1,7 @@
 package com.diipl.moviebeam.ui.casting
 
+import android.annotation.SuppressLint
+import android.webkit.WebSettings
 import android.webkit.WebView
 import com.diipl.moviebeam.Constants
 import com.diipl.moviebeam.databinding.ActivityCastingBinding
@@ -12,10 +14,14 @@ class CastingActivity : BaseActivity() {
 
     override fun observeViewModel() {}
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun initViewBinding() {
         binding = ActivityCastingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val webView: WebView = binding.wvCasting
+        webView.clearCache(true)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
         webView.loadUrl(Constants.CASTING_URL)
     }
 
