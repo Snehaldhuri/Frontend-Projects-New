6a9ea1a992862eff49c19bb866de61af430c4347
snehal dhuri <snehal.dhuri@diipl.com>
2024-02-01 19:36:17 +0530

    manmenu video changes

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/mainmenu/MainMenuActivity.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/mainmenu/MainMenuActivity.kt
@@ -238,11 +238,8 @@ class MainMenuActivity : BaseActivity() {
             is Resource.Success -> {
                 val response = mainMenuViewModel.accountSetupLiveData.value?.data
 
-//                HOTEL_VIDEO_URL =
-//                    response?.httpStreamingHotelvideoUrl + response?.hotelChannelList?.get(0)?.fileName
-
                 HOTEL_VIDEO_URL =
-                    response?.httpStreamingHotelvideoUrl + "15712_HotelVideo.m2t"
+                    response?.httpStreamingHotelvideoUrl + response?.hotelChannelList?.get(0)?.fileName
 
                 initializePlayer()
 
