e121d195365840301adbf561e7ccd77c379b7e46
snehal dhuri <snehal.dhuri@diipl.com>
2024-02-05 17:46:02 +0530

    moviebeam la flip animation

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/localattraction/LaCardAdapter.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/localattraction/LaCardAdapter.kt
@@ -1,18 +1,25 @@
 package com.diipl.moviebeam.ui.localattraction
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.diipl.moviebeam.R
 import com.diipl.moviebeam.data.dto.localattraction.LAService
+import com.diipl.moviebeam.ui.guestservice.localAttraction.LaCardAdapterGs
 import com.diipl.moviebeam.utils.loadImagesWithGlideExtLA
 
 class LaCardAdapter(
@@ -23,40 +30,23 @@ class LaCardAdapter(
     private var gradientDrawable: GradientDrawable? = null
 
     private var itemList: List<LAService> = mutableListOf()
-
+    var isFront = true
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.iv_la_card_image)
         val textView: TextView = itemView.findViewById(R.id.tv_la_card_title)
         val description: TextView = itemView.findViewById(R.id.tv_la_card_desc)
         val frontCard: CardView = itemView.findViewById(R.id.front_card)
+
+        val backCard: CardView = itemView.findViewById(R.id.back_card)
+        val flipButton: Button = itemView.findViewById(R.id.btn_MoreInfo)
+
+        val okButton: Button = itemView.findViewById(R.id.btn_backInfo)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.la_carousel, parent, false)
-        view.isClickable = true
 
-        view.findViewById<CardView>(R.id.front_card).background.setTint(
-            Color.parseColor(
-                defaultColor
-            )
-        )
-        view.setOnFocusChangeListener { it, hasFocus ->
-            if (hasFocus) {
-                it.findViewById<CardView>(R.id.front_card).background = gradientDrawable
-
-                val scaleX = ObjectAnimator.ofFloat(it, View.SCALE_X, 1.0f, 1.02f)
-                val scaleY = ObjectAnimator.ofFloat(it, View.SCALE_Y, 1.0f, 1.02f)
-
-                val scaleAnimatorSet = AnimatorSet()
-                scaleAnimatorSet.duration = 200
-                scaleAnimatorSet.playTogether(scaleX, scaleY)
-                scaleAnimatorSet.start()
-            } else {
-                it.scaleX = 1.0f
-                it.scaleY = 1.0f
-            }
-        }
         return MyViewHolder(view)
     }
 
@@ -69,7 +59,75 @@ class LaCardAdapter(
         holder.textView.text = item.title
         holder.description.text = item.description.replace("<br/>", "")
 
-        holder.frontCard.visibility = View.VISIBLE
+        holder.flipButton.setOnClickListener {
+            Log.e("onBindViewHolder:", "flipButton")
+            flipImage(holder.frontCard, holder.backCard)
+        }
+
+        holder.okButton.setOnClickListener {
+            Log.e("onBindViewHolder:", "okButton")
+            unFlipImage(holder.frontCard, holder.backCard)
+        }
+        holder.flipButton.requestFocus()
+        holder.flipButton.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                holder.frontCard.isClickable = false
+                holder.flipButton.background = gradientDrawable
+
+                val scaleX = ObjectAnimator.ofFloat(holder.itemView, View.SCALE_X, 1.0f, 1.02f)
+                val scaleY = ObjectAnimator.ofFloat(holder.itemView, View.SCALE_Y, 1.0f, 1.02f)
+
+                val scaleAnimatorSet = AnimatorSet()
+                scaleAnimatorSet.duration = 200
+                scaleAnimatorSet.playTogether(scaleX, scaleY)
+                scaleAnimatorSet.start()
+            } else {
+                holder.itemView.scaleX = 1.0f
+                holder.itemView.scaleY = 1.0f
+            }
+        }
+        holder.flipButton.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                        if (isFront) {
+                            flipImage(holder.frontCard, holder.backCard)
+                        } else {
+                            unFlipImage(holder.frontCard, holder.backCard)
+                        }
+                        return@setOnKeyListener true
+                    }
+                }
+            }
+            return@setOnKeyListener false
+        }
+
+
+    }
+    private fun flipImage(back: CardView, front: CardView) {
+        val flipAnimator = ObjectAnimator.ofFloat(back, "rotationY", 0f, 360f)
+        flipAnimator.duration = 500
+        flipAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                back.visibility = View.GONE
+                front.visibility = View.VISIBLE
+            }
+        })
+        flipAnimator.start()
+        isFront=false
+    }
+
+    private fun unFlipImage(back: CardView, front: CardView) {
+        val unFlipAnimator = ObjectAnimator.ofFloat(front, "rotationY", 360f, 0f)
+        unFlipAnimator.duration = 500
+        unFlipAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                front.visibility = View.GONE
+                back.visibility = View.VISIBLE
+            }
+        })
+        unFlipAnimator.start()
+        isFront=true
     }
 
     fun setGradientDrawable(gradient: GradientDrawable) {
@@ -78,7 +136,6 @@ class LaCardAdapter(
 
     fun setList(itemList: List<LAService>) {
         this.itemList = itemList
-    }
-}
+    }}
 
 
