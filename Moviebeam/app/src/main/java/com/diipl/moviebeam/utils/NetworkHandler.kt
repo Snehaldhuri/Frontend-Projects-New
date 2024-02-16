46ba0127a9123e0112109339b179388a3305ceea
Siddhesh Hatiskar <siddheshh@diipl.com>
2024-02-16 16:04:29 +0530

    Fixes...

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/utils/NetworkHandler.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/utils/NetworkHandler.kt
@@ -1,6 +1,5 @@
 package com.diipl.moviebeam.utils
 
-import com.diipl.moviebeam.Constants
 import com.diipl.moviebeam.data.Resource
 import retrofit2.Response
 import javax.inject.Inject
@@ -12,16 +11,16 @@ open class NetworkHandler @Inject constructor(
 ) {
     suspend fun <T> safeAPiCall(api: suspend () -> Response<T>): Resource<T> {
         try {
-            if (networkUtil.isNetworkConnected) {
-                val result = api()
-                if (result.isSuccessful) {
-                    val body = result.body()
-                    return Resource.Success(body)
-                }
-                return Resource.DataError(msg = result.message(), code = result.code())
-            } else {
-                return Resource.DataError(msg = Constants.INTERNET_ERROR_MESSAGE)
+//            if (networkUtil.isNetworkConnected) {
+            val result = api()
+            if (result.isSuccessful) {
+                val body = result.body()
+                return Resource.Success(body)
             }
+            return Resource.DataError(msg = result.message(), code = result.code())
+//            } else {
+//                return Resource.DataError(msg = Constants.INTERNET_ERROR_MESSAGE)
+//            }
         } catch (e: Exception) {
             return Resource.DataError(msg = e.message)
         }
