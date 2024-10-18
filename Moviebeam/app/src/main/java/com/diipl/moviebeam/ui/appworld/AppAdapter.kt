428ff0b31ccf5ca0c0dc8abf5a52bf9249baa41b
Gitesh Gaonkar <giteshg@diipl.in>
2024-10-18 19:37:45 +0530

    google play games icon issue handled

--- Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/appworld/AppAdapter.kt
+++ Moviebeam/app/src/main/java/com/diipl/moviebeam/ui/appworld/AppAdapter.kt
@@ -1,6 +1,11 @@
 package com.diipl.moviebeam.ui.appworld
 
+import android.content.Context
+import android.content.Intent
 import android.content.pm.ApplicationInfo
+import android.content.pm.ResolveInfo
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.animation.Animation
@@ -51,13 +56,62 @@ class AppAdapter(
             }
         }
         holder.binding.tvAppName.text = context.packageManager.getApplicationLabel(item)
-        holder.binding.ivAppIcon.setImageDrawable(context.packageManager.getApplicationBanner(item))
+        Log.e("TAG", "onBindViewHolder: " + context.packageManager.getApplicationBanner(item))
+        //var banner: Drawable = app.activityInfo.loadBanner(mPackageManager)
+        //if (banner == null) {
+        //    banner = app.activityInfo.applicationInfo.loadBanner(mPackageManager)
+        //}
+
+        val appsIntent = Intent(Intent.ACTION_MAIN, null).apply {
+            addCategory(Intent.CATEGORY_LEANBACK_LAUNCHER)
+        }
+        val apps: List<ResolveInfo> = context.packageManager.queryIntentActivities(appsIntent, 0)
+        Log.e("TAG" , "onBindViewHolder: ${apps.size} $apps", )
+        getBanner(context,item.packageName)
+        if(context.packageManager.getApplicationBanner(item)==null){
+            var icon=getBanner(context,item.packageName)
+            if(icon!=null) {
+                holder.binding.ivAppIcon.setImageDrawable(icon)
+            }else{
+                holder.binding.ivAppIcon.setImageDrawable(
+                    context.packageManager.getApplicationIcon(
+                        item
+                    )
+                )
+            }
+
+        }else {
+            holder.binding.ivAppIcon.setImageDrawable(
+                context.packageManager.getApplicationBanner(
+                    item
+                )
+            )
+        }
         holder.binding.cardApp.setOnClickListener {
             if((item.packageName != Constants.NETFLIX_PACKAGE_NAME) || (item.packageName == Constants.NETFLIX_PACKAGE_NAME && GuestDetails.IS_GUEST_CHECKED_IN))
                 onItemClicked(item)
             else
                 context.showToast(context.getString(R.string.please_contact_the_front_desk_for_assistance))
         }
+    }
+
+    private fun getBanner(context: Context, packageName: String): Drawable? {
+        var banner: Drawable?=null
+        val appsIntent = Intent(Intent.ACTION_MAIN, null).apply {
+            addCategory(Intent.CATEGORY_LEANBACK_LAUNCHER)
+        }
+        val apps: List<ResolveInfo> = context.packageManager.queryIntentActivities(appsIntent, 0)
+
+        for (app in apps) {
+            if(app.activityInfo.packageName==packageName) {
+
+                banner = app.activityInfo.loadBanner(context.packageManager)
+                if (banner == null) {
+                    banner = app.activityInfo.applicationInfo.loadBanner(context.packageManager)
+                }
+            }
+        }
+        return banner
     }
 
     fun setAppList(appList: List<ApplicationInfo>) {
